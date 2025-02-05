609bf8b41ce77b1fd893144a61f6d28065a7972a
JuanPabloRiglos <juan.riglos@hotmail.com>
2025-02-05 10:29:33 -0300

    Add deploy changes

--- db/config.js
+++ db/config.js
@@ -5,12 +5,19 @@ const PASSWORD = encodeURIComponent(config.dbPassword);
 const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 export default {
-    development:{
-        url:URI,
-        dialect: 'postgres'
+  development: {
+    url: URI,
+    dialect: 'postgres',
+  },
+  production: {
+    url: URI,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Solo para desarrollo; quítalo en producción si puedes
+      },
     },
-    production: {
-        url:URI,
-        dialect: 'postgres'
-    }
-}
+    loggin: false, // Evitar mostrar logs en producción
+  },
+};
